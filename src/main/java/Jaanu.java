7a97232ddbcee5dbb9774c5f702e2d52560793c1
jainamashah <1106jainam@gmail.com>
2026-01-23 16:05:24 +0800

    Jaanu.java: Rename from Duke and revise greeting message

---
+++ src/main/java/Jaanu.java
@@ -0,0 +1,11 @@
+public class Jaanu {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + " Hello! I'm Jaanu\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(logo);
+    }
+}
