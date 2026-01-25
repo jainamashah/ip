0f362b9364ead79f2c45025276d863c83b10f956
jainamashah <1106jainam@gmail.com>
2026-01-25 11:36:55 +0800

    Jaanu.java: Personalize chatbot and add input functionality

--- src/main/java/Jaanu.java
+++ src/main/java/Jaanu.java
@@ -1,11 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Jaanu {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-                + " Hello! I'm Jaanu\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(logo);
+        String dividerLine = "____________________________________________________________\n";
+        String Greeting = dividerLine
+                + " I'm your Jaanu\n"
+                + " I'll do anything for you my love, just ask and see ;-) \n"
+                + dividerLine;
+        String Bye = dividerLine+
+                " how dare you, hope to never see you again. bye :-( \n"
+                + dividerLine;
+        Scanner in = new Scanner(System.in);
+        System.out.println(Greeting);
+        String input = in.nextLine() +"\n";
+        while (!input.contains("bye")){
+            System.out.println(dividerLine+input+dividerLine);
+            input = in.nextLine()+"\n";
+        }
+        System.out.println(Bye);
     }
 }
