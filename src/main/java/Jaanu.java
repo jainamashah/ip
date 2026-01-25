6611d62ba0fc902b25cdd178031c76bf6b17fec8
jainamashah <1106jainam@gmail.com>
2026-01-25 17:40:32 +0800

    Jaanu.java: Add manageTasks functionality and refactor code - Chatbot can now store tasks when inputted by user. - Chatbot responds to 'list' command by listing all tasks inputted by   user. - Refactor code to use OOP by adding printGreeting, printBye, printList   to ensure readibility and better debugging.

--- src/main/java/Jaanu.java
+++ src/main/java/Jaanu.java
@@ -2,22 +2,56 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Jaanu {
-    public static void main(String[] args) {
-        String dividerLine = "____________________________________________________________\n";
+    private static int numOfTasks = 0;
+    private static String[] tasks = new String[100];
+    private static final String dividerLine = "____________________________________________________________\n";
+
+    public static void printList(){
+        for (int i = 0; i< numOfTasks ;i++){
+            System.out.println((i+1)+"."+tasks[i]);
+        }
+    }
+
+    public static void printGreeting(){
         String Greeting = dividerLine
                 + " I'm your Jaanu\n"
-                + " I'll do anything for you my love, just ask and see ;-) \n"
+                + " I'll do anything, just ask and see \n"
                 + dividerLine;
-        String Bye = dividerLine+
-                " how dare you, hope to never see you again. bye :-( \n"
-                + dividerLine;
-        Scanner in = new Scanner(System.in);
         System.out.println(Greeting);
-        String input = in.nextLine() +"\n";
-        while (!input.contains("bye")){
-            System.out.println(dividerLine+input+dividerLine);
-            input = in.nextLine()+"\n";
-        }
+    }
+
+    public static void printBye(){
+        String Bye = dividerLine+
+                "hope to never see you again. bye \n"
+                + dividerLine;
         System.out.println(Bye);
+    }
+
+    public static void manageTasks(){
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        if (!input.contains("bye")){
+            tasks[numOfTasks] = input;
+        }
+        else{
+            return;
+        }
+        while (!input.contains("bye")){
+            if (!input.equals("list")) {
+                tasks[numOfTasks] = input;
+                numOfTasks++;
+                System.out.println(dividerLine + "added: " + input + "\n" + dividerLine + "\n");
+            }
+            else {
+                printList();
+            }
+            input = in.nextLine();
+        }
+    }
+
+    public static void main(String[] args) {
+        printGreeting();
+        manageTasks();
+        printBye();
     }
 }
