31adf85b3f8998e4e7e3aaa3c8f0127e8aeba770
jainamashah <1106jainam@gmail.com>
2026-02-28 01:41:34 +0700

    Feat: Add auto-save and load for task list
    - Save tasks to disk automatically whenever the task list changes
    - Load saved tasks from disk on chatbot startup
    - Use a hard-coded data file path (e.g., ./data/duke.txt)

--- src/main/java/jaanu/ui/Jaanu.java
+++ src/main/java/jaanu/ui/Jaanu.java
@@ -1,29 +1,134 @@
 package jaanu.ui;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 import jaanu.Task.Task;
 import jaanu.Task.Deadline;
 import jaanu.Task.Event;
 import jaanu.JaanuException.JaanuException;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+
+
 public class Jaanu {
-    private static final int MAX_TASKS = 100;
     private static final int PARSED_ARGS_SIZE = 3;
     private static final String DIVIDER_LINE = "____________________________________________________________\n";
 
-    private static int numOfTasks = 0;
-    private static Task[] taskList = new Task[MAX_TASKS];
-    private static String[] parsedArgs = new String[PARSED_ARGS_SIZE];
+    // A-Collections: use Java Collections
+    private static final List<Task> tasks = new ArrayList<>();
+    private static final String[] parsedArgs = new String[PARSED_ARGS_SIZE];
+
+    //Relative path for stored file
+    private static final String filePath = "jaanu.txt";
 
     public static void printList() throws JaanuException {
-        if (numOfTasks == 0) {
+        if (tasks.isEmpty()) {
             throw new JaanuException("bruh your list is empty lol. add something first babes");
         }
-        for (int i = 0; i < numOfTasks; i++) {
-            String doneChar = taskList[i].getStatusIcon();
-            String classChar = taskList[i].getClassIcon();
-            System.out.println((i + 1) + ". [" + classChar + "] [" + doneChar + "] "
-                    + taskList[i].toStringMain());
+
+        for (int i = 0; i < tasks.size(); i++) {
+            Task t = tasks.get(i);
+            String doneChar = t.getStatusIcon();
+            String classChar = t.getClassIcon();
+            System.out.println((i + 1) + ". " + t.toString());
+        }
+    }
+
+    private static void printFileContents(String filePath) throws FileNotFoundException {
+        File f = new File(filePath); // create a File for the given file path
+        Scanner s = new Scanner(f); // create a Scanner using the File as the source
+        while (s.hasNext()) {
+            System.out.println(s.nextLine());
+        }
+    }
+
+    public static void loadTasks(String input) throws JaanuException{
+        String[] unparsedTaskList = input.split("\n");
+        String classChar, doneChar, description, input_task;
+        int numOfTasks = unparsedTaskList.length;
+        if (!(input.contains("\n"))) {
+            numOfTasks = 0;
+        }
+        for (int i = 0; i < numOfTasks ; i++) {
+            input_task = unparsedTaskList[i];
+            Task t;
+            String[] input_fields = input_task.split("/");
+            //fine che
+            classChar = input_fields[0];
+            doneChar = input_fields[1];
+            description = input_fields[2];
+            switch (classChar) {
+            case "T":
+                t = new Task(description); // "todo "
+                tasks.add(t);
+                break;
+            case "D":
+                String by = input_fields[3];
+                t = new Deadline(description, by); // "todo "
+                tasks.add(t);
+                break;
+            case "E":
+                String from = input_fields[3];
+                String to = input_fields[4];
+                t = new Event(description, from, to); // "todo "
+                tasks.add(t);
+                break;
+            default:
+                throw new JaanuException("logic error");
+            }
+            if (doneChar.equals("X")) {
+                t.setAsDone();
+            }
+        }
+    }
+
+    private static void updateFile() throws JaanuException {
+        try {
+            FileWriter fw = new FileWriter(filePath);
+            for (int i = 0; i < tasks.size(); i++) {
+                String temp;
+                String lineToWrite;
+                switch (tasks.get(i).getClassIcon()){
+                case "T" :
+                    Task t = tasks.get(i);
+                    temp = "";
+                    lineToWrite = t.getClassIcon() + "/" + t.getStatusIcon() + "/" + t.toStringMain() +temp +"\n" ;
+                    break;
+                case "D" :
+                    Deadline d = (Deadline) tasks.get(i);
+                    temp = "/"+d.getBy();
+                    lineToWrite = d.getClassIcon() + "/" + d.getStatusIcon() + "/" + d.toStringMain() +temp +"\n" ;
+                    break;
+                case "E" :
+                    Event e = (Event) tasks.get(i);
+                    temp = "/"+e.getFrom()+"/"+e.getBy();
+                    lineToWrite = e.getClassIcon() + "/" + e.getStatusIcon() + "/" + e.toStringMain() +temp +"\n" ;
+                    break;
+                default:
+                    throw new JaanuException("internal logic error");
+                }
+                try {
+                    fw.write(lineToWrite);
+                    printFileContents(filePath);
+                }
+                catch (IOException e){
+                    throw new JaanuException("babes there is some issue with file writing");
+                }
+            }
+            try {
+                fw.close();
+            }
+            catch (IOException e){
+                throw new JaanuException("babes there is some issue with file writing");
+            }
+        }
+        catch (IOException e) {
+            throw new JaanuException("babes there is some issue with file writing");
         }
     }
 
@@ -42,20 +147,21 @@ public class Jaanu {
         System.out.println(bye);
     }
 
-    public static void printAddConfirmationMsg() {
-        String msg = DIVIDER_LINE
-                + "um ok, there you go babes:";
-        System.out.println(msg);
-        System.out.println(taskList[numOfTasks].toString());
-        String remainingTaskMsg = "Now you have " + (numOfTasks + 1) + " tasks in the list.\n"
-                + DIVIDER_LINE;
-        System.out.println(remainingTaskMsg);
-    }
-
     private static void printError(String message) {
         System.out.println(DIVIDER_LINE
                 + message + "\n"
                 + DIVIDER_LINE);
+    }
+
+    private static void printAddConfirmationMsg(Task addedTask) {
+        String msg = DIVIDER_LINE
+                + "um ok, there you go babes:";
+        System.out.println(msg);
+        System.out.println(addedTask.toString());
+
+        String remainingTaskMsg = "Now you have " + tasks.size() + " tasks in the list.\n"
+                + DIVIDER_LINE;
+        System.out.println(remainingTaskMsg);
     }
 
     public static void markTask(String[] inputString) throws JaanuException {
@@ -70,18 +176,20 @@ public class Jaanu {
             throw new JaanuException("that ain't even a number lol. use mark 1 or mark 2 etc");
         }
 
-        if (taskNum < 0 || taskNum >= numOfTasks) {
+        if (taskNum < 0 || taskNum >= tasks.size()) {
             throw new JaanuException("lmao task " + (taskNum + 1) + " doesn't exist. u only got "
-                    + numOfTasks + " task(s) babes");
+                    + tasks.size() + " task(s) babes");
         }
 
-        if (taskList[taskNum].isDone()) {
+        Task t = tasks.get(taskNum);
+
+        if (t.isDone()) {
             throw new JaanuException("bro u already marked that one. getting old?");
         }
 
-        taskList[taskNum].setAsDone();
+        t.setAsDone();
         System.out.println(DIVIDER_LINE + "Attaboy, keep the grind on");
-        System.out.println("  [X] " + taskList[taskNum].toStringMain() + "\n" + DIVIDER_LINE);
+        System.out.println("  [X] " + t.toStringMain() + "\n" + DIVIDER_LINE);
     }
 
     public static void unmarkTask(String[] inputString) throws JaanuException {
@@ -96,18 +204,20 @@ public class Jaanu {
             throw new JaanuException("bruh that's not a number. try unmark 1 or unmark 2 etc");
         }
 
-        if (taskNum < 0 || taskNum >= numOfTasks) {
+        if (taskNum < 0 || taskNum >= tasks.size()) {
             throw new JaanuException("nice try but task " + (taskNum + 1) + " doesn't exist. u only got "
-                    + numOfTasks + " task(s)");
+                    + tasks.size() + " task(s)");
         }
 
-        if (!taskList[taskNum].isDone()) {
+        Task t = tasks.get(taskNum);
+
+        if (!t.isDone()) {
             throw new JaanuException("it's already unmarked genius. pay attention babes");
         }
 
-        taskList[taskNum].setAsNotDone();
+        t.setAsNotDone();
         System.out.println(DIVIDER_LINE + "U lazy dog, go finish this task:");
-        System.out.println("  [ ] " + taskList[taskNum].toStringMain() + "\n" + DIVIDER_LINE);
+        System.out.println("  [ ] " + t.toStringMain() + "\n" + DIVIDER_LINE);
     }
 
     public static void parseArgs(String input) {
@@ -132,11 +242,6 @@ public class Jaanu {
     }
 
     private static void addTask(String command, int taskDescriptionLength) throws JaanuException {
-        if (numOfTasks >= MAX_TASKS) {
-            throw new JaanuException("woah slow down babes, ur list is full (" + MAX_TASKS + " max). delete something first");
-        }
-
-        // Determine the command length to extract description
         int commandLength;
         switch (command) {
         case "todo":
@@ -152,7 +257,6 @@ public class Jaanu {
             throw new JaanuException("idk what a " + command + " is babes");
         }
 
-        // Check if there's a description after the command
         if (parsedArgs[0].length() <= commandLength) {
             throw new JaanuException("bro u can't just say '" + command + "' and leave me hanging\n"
                     + " tell me WHAT u wanna add. example: " + command + " buy flowers for jaanu");
@@ -164,9 +268,11 @@ public class Jaanu {
                     + " try: " + command + " take jaanu on a date");
         }
 
+        Task newTask;
+
         switch (command) {
         case "todo":
-            taskList[numOfTasks] = new Task(description);
+            newTask = new Task(description);
             break;
 
         case "deadline":
@@ -175,7 +281,7 @@ public class Jaanu {
                         + " deadline <task> / <date>\n"
                         + " example: deadline confess to jaanu / tonight");
             }
-            taskList[numOfTasks] = new Deadline(description, parsedArgs[2].trim());
+            newTask = new Deadline(description, parsedArgs[2].trim());
             break;
 
         case "event":
@@ -189,18 +295,18 @@ public class Jaanu {
                         + " event <what> / <from> / <to>\n"
                         + " example: event netflix with jaanu / 8pm / whenever ;)");
             }
-            taskList[numOfTasks] = new Event(description, parsedArgs[1].trim(), parsedArgs[2].trim());
+            newTask = new Event(description, parsedArgs[1].trim(), parsedArgs[2].trim());
             break;
 
         default:
             throw new JaanuException("nah I can't add that: " + command);
         }
 
-        printAddConfirmationMsg();
-        numOfTasks++;
+        tasks.add(newTask);
+        printAddConfirmationMsg(newTask);
     }
 
-    public static void manageTasks() {
+    public static void manageTasks() throws JaanuException {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
 
@@ -235,7 +341,7 @@ public class Jaanu {
                     break;
                 default:
                     throw new JaanuException("wht is '" + command + "'??? I don't speak that language babes\n"
-                            + " try: list, todo, deadline, event, mark, unmark, bye");
+                            + " try: list, todo, deadline, event, mark, unmark, delete, bye");
                 }
 
             } catch (JaanuException e) {
@@ -244,13 +350,69 @@ public class Jaanu {
                 printError("yikes something broke: " + e.getMessage());
             }
 
+            try {
+                updateFile();
+            }
+            catch (JaanuException e){
+                printError(e.getMessage());
+            }
+
             input = in.nextLine();
         }
-
         in.close();
     }
 
-    public static void main(String[] args) {
+    private static String readFileContents(String filePath) throws FileNotFoundException {
+        File f = new File(filePath); // create a File for the given file path
+        Scanner s = new Scanner(f); // create a Scanner using the File as the source
+        String output = "";
+        while (s.hasNext()) {
+            output = output + s.nextLine()+"\n";
+        }
+        return output;
+    }
+
+    public static String safelyInitFile() {
+        try {
+            File f = new File(filePath); // create a File for the given file path
+
+            String output = "";
+
+            if (!(f.createNewFile())){
+                output = readFileContents(filePath);
+                return output;
+            }
+            else {
+                return output;
+            }
+        }
+        catch (IOException e){
+            printError("An IO Error Occurred.");
+        }
+        return "";
+    }
+
+    public static void cleanFile() throws JaanuException {
+        try {
+            FileWriter fw = new FileWriter(filePath);
+
+            try {
+                fw.write("");
+            }
+            catch (IOException e) {
+                throw new JaanuException("babes there is some issue with file writing");
+            }
+        }
+        catch (JaanuException e){
+            printError(e.getMessage());
+        }
+        catch (IOException e){
+            printError("An IO Error Occurred.");
+        }
+    }
+
+    public static void main(String[] args) throws JaanuException {
+        loadTasks(safelyInitFile());
         printGreeting();
         manageTasks();
         printBye();
